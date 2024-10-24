8242f7182f9683b8c7b1b31cfb8890b68d2a16a6
sashapoliti <sas.politi@gmail.com>
2024-10-24 10:32:46 +0200

    done api dipendenti

--- GeestioneValutazioni/force-app/main/default/lwc/storicoValutazioni/storicoValutazioni.js
+++ GeestioneValutazioni/force-app/main/default/lwc/storicoValutazioni/storicoValutazioni.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
+import getDipendenti from '@salesforce/apex/StoricoClass.getDipendenti';
 
-export default class StoricoValutazioni extends LightningElement {}
+export default class StoricoValutazioni extends LightningElement {
+
+    dipendentiList;
+
+    /* metodo che si richiama al caricamento del componente */
+    connectedCallback() {
+        this.get();
+    }
+
+    /* ricerca dei contatti */
+    async get() {
+        try {
+            this.dipendentiList = await getDipendenti();
+
+            console.log(this.dipendentiList);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
